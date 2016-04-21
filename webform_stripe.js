5e3ab9936aee6ba9ec63b19a2d16d84b301f947e
Joel Stein <joel@onfiremedia.com>
2016-04-21 18:09:37 -0500

    Issue #2661330 by joelstein: Added additional step to form to ensure user click and prevent popup blockers.

--- webform_stripe.js
+++ webform_stripe.js
@@ -4,17 +4,36 @@
    * Ajax command to open Stripe Checkout, store result, and submit form.
    */
   Drupal.ajax.prototype.commands.stripeCheckout = function(ajax, data, status) {
-    // Open Stripe Checkout.
-    StripeCheckout.open($.extend(data.params, {
-      // Payment was successful.
-      token: function(token) {
-        // Set token and email in token field.
-        $(data.selector, ajax.form.context).val(token.id + ':' + token.email);
+    // Wrap and hide form.
+    var $form = $(ajax.form[0]);
+    $wrapper = $('<div class="webform-stripe-wrapper"></div>').hide();
+    $form.wrapInner($wrapper);
 
-        // Submit form.
-        ajax.form[0].submit();
-      }
-    }));
+    // Append action buttons.
+    $form.append(data.actions);
+
+    // When pay button is clicked, open Stripe Checkout.
+    var $payButton = $('.webform-stripe-pay-button', $form);
+    $payButton.click(function() {
+      StripeCheckout.open($.extend(data.params, {
+        // Payment was successful.
+        token: function(token) {
+          // Set token and email in token field.
+          $(data.selector, ajax.form.context).val(token.id + ':' + token.email);
+
+          // Submit form.
+          ajax.form[0].submit();
+        }
+      }));
+      return false;
+    });
+
+    // When edit button is clicked, remove action buttons and unwrap form.
+    $('.webform-stripe-edit-button', $form).click(function() {
+      $('.webform-stripe-actions').remove();
+      $form.children().children().unwrap();
+      return false;
+    });
   };
 
 }(jQuery));
