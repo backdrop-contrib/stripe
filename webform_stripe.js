c6028748f209225e9fadd7aca69c53d5bec4bf78
Joel Stein <joel@onfiremedia.com>
2016-04-22 11:14:59 -0500

    Issue #2711789 by joelstein: Updated form manipulation to leverage Drupal's form & ajax API.

--- webform_stripe.js
+++ webform_stripe.js
@@ -1,39 +1,24 @@
 (function($) {
 
   /**
-   * Ajax command to add Stripe Checkout buttons to a Webform.
+   * Opens Stripe Checkout when Webform Stripe 'pay' button is clicked.
    */
-  Drupal.ajax.prototype.commands.webformStripeCheckout = function(ajax, data, status) {
-    // Wrap and hide form.
-    var $form = $(ajax.form[0]);
-    $wrapper = $('<div class="webform-stripe-wrapper"></div>').hide();
-    $form.wrapInner($wrapper);
+  Drupal.behaviors.webformStripe = {
+    attach: function(context, settings) {
+      $('.webform-stripe-pay', context).click(function() {
+        StripeCheckout.open($.extend(settings.webform_stripe, {
+          // Payment was successful.
+          token: function(token) {
+            // Set token and email in token field.
+            $('.webform-stripe-token', context).val(token.id + ':' + token.email);
 
-    // Append action buttons.
-    $form.append(data.actions);
-
-    // When pay button is clicked, open Stripe Checkout.
-    var $payButton = $('.webform-stripe-pay-button', $form);
-    $payButton.click(function() {
-      StripeCheckout.open($.extend(data.params, {
-        // Payment was successful.
-        token: function(token) {
-          // Set token and email in token field.
-          $(data.selector, ajax.form.context).val(token.id + ':' + token.email);
-
-          // Submit form.
-          ajax.form[0].submit();
-        }
-      }));
-      return false;
-    });
-
-    // When edit button is clicked, remove action buttons and unwrap form.
-    $('.webform-stripe-edit-button', $form).click(function() {
-      $('.webform-stripe-actions').remove();
-      $form.children().children().unwrap();
-      return false;
-    });
+            // Submit form.
+            $('.webform-submit', context).click();
+          }
+        }));
+        return false;
+      });
+    }
   };
 
 }(jQuery));
