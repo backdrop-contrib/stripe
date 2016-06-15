2a0b71407cae24fbc93bc72973ad81ecddeaacdf
Joel Stein <joel@onfiremedia.com>
2016-06-15 10:40:48 -0500

    Issue #2749231 by joelstein: Scroll to top of form after ajax replaces its contents.

--- webform_stripe.js
+++ webform_stripe.js
@@ -1,6 +1,30 @@
 (function($) {
 
   /**
+   * Scrolls to top of Webform with Stripe payment.
+   */
+  Drupal.ajax.prototype.commands.webformStripeScroll = function(ajax, response, status) {
+    // Determine what to scroll to, either our wrapper div, or a block, or a
+    // Panels pane.
+    var $el = $(response.selector).closest('.block.block-webform');
+    if (!$el.length) {
+      $el = $(response.selector).closest('.panel-pane.pane-block[class*="pane-webform-client-block-"');
+      if (!$el.length) {
+        $el = $(response.selector);
+      }
+    }
+
+    // Get element top, and offset it if Adminimal Menu is in use.
+    var top = $el.offset().top;
+    if ($('body').hasClass('adminimal-menu')) {
+      top -= 29;
+    }
+
+    // Scroll!
+    $('html, body').animate({scrollTop: top}, response.duration);
+  };
+
+  /**
    * Opens Stripe Checkout when Webform Stripe 'pay' button is clicked.
    */
   Drupal.behaviors.webformStripe = {
