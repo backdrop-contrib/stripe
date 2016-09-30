8c5beb5f7397c1e31755507b1c1ab4e94909ef03
Joel Stein <joel@onfiremedia.com>
2016-09-30 13:07:57 -0500

    Issue #2782783 by joelstein: Restored immediate payment form, thanks to 'synchronous' ajax.

--- webform_stripe.js
+++ webform_stripe.js
@@ -1,48 +1,31 @@
 (function($) {
 
   /**
-   * Scrolls to top of Webform with Stripe payment.
+   * Overrides Ajax beforeSubmit callback to make request synchronous, which
+   * preserves the click event after the ajax response is received. This
+   * prevents popup blockers from "breaking" our checkout experience.
+   * @see https://www.drupal.org/node/2661330
+   * @see https://www.drupal.org/node/2782783
    */
-  Drupal.ajax.prototype.commands.webformStripeScroll = function(ajax, response, status) {
-    // Determine what to scroll to, either our wrapper div, or a block, or a
-    // Panels pane.
-    var $el = $(response.selector).closest('.block.block-webform');
-    if (!$el.length) {
-      $el = $(response.selector).closest('.panel-pane.pane-block[class*="pane-webform-client-block-"]');
-      if (!$el.length) {
-        $el = $(response.selector);
-      }
+  var beforeSubmit = Drupal.ajax.prototype.beforeSubmit;
+  Drupal.ajax.prototype.beforeSubmit = function(form_values, element, options) {
+    if (element.hasClass('webform-stripe-form')) {
+      options.async = false;
     }
-
-    // Get element top, and offset it if Adminimal Menu is in use.
-    var top = $el.offset().top;
-    if ($('body').hasClass('adminimal-menu')) {
-      top -= 29;
-    }
-
-    // Scroll!
-    $('html, body').animate({scrollTop: top}, response.duration);
+    beforeSubmit();
   };
 
   /**
-   * Opens Stripe Checkout when Webform Stripe 'pay' button is clicked.
+   * Ajax command to open Stripe Checkout, store token and email in hidden
+   * token field, and submit form.
    */
-  Drupal.behaviors.webformStripe = {
-    attach: function(context, settings) {
-      $('.webform-stripe-pay', context).click(function() {
-        StripeCheckout.open($.extend(settings.webform_stripe, {
-          // Payment was successful.
-          token: function(token) {
-            // Set token and email in token field.
-            $('.webform-stripe-token', context).val(token.id + ':' + token.email);
-
-            // Submit form.
-            $('.webform-submit', context).click();
-          }
-        }));
-        return false;
-      });
-    }
+  Drupal.ajax.prototype.commands.webformStripeCheckout = function(ajax, data, status) {
+    StripeCheckout.open($.extend(data.params, {
+      token: function(token) {
+        $('.webform-stripe-token', ajax.form.context).val(token.id + ':' + token.email);
+        ajax.form[0].submit();
+      }
+    }));
   };
 
 }(jQuery));
