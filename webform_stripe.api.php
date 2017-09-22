3ece8c9d21fb90cdc07e8c2c8768fe7379db669f
Joel Stein <joel@onfiremedia.com>
2017-09-22 13:26:46 -0500

    Issue #2899976 by eddie_c, joelstein: Improved usage of hook_webform_stripe_checkout_settings_alter().

--- webform_stripe.api.php
+++ webform_stripe.api.php
@@ -8,15 +8,32 @@
  * @param $context
  *   An array of contextual information with the following keys:
  *   - node: The Webform node object.
- *   - component: The Webform component array.
- *   - data: The array of form values submitted by the user.
+ *   - component: The Webform component array for the Stripe payment component.
+ *   - data: The array of submitted form values (see "state" for how this is
+ *     keyed).
+ *   - form: The array of submitted form values, keyed by form key.
+ *   - state: One of the following values:
+ *     - WEBFORM_STRIPE_SETTINGS_CHECKOUT: Before opening Stripe Checkout.
+ *       The 'data' array is keyed by component's 'form_key'.
+ *     - WEBFORM_STRIPE_SETTINGS_PROCESS: After user submits Stripe Checkout
+ *       form and before server processes Stripe charge/subscription.
+ *       The 'data' array is keyed by component's 'cid'.
  */
 function hook_webform_stripe_checkout_settings_alter(&$settings, &$context) {
   // Change Stripe Checkout title to Webform title.
-  $settings['name'] = $node->title;
+  $settings['name'] = $context['node']->title;
 
   // Change Stripe Checkout currency based on a "Country" component value.
-  if (isset($data['country']) && $data['country'] === 'us') {
+  if (isset($context['form']['country']) && $context['form']['country'] === 'us') {
     $settings['currency'] = 'USD';
+  }
+
+  // Dynamically calculate amount with custom logic. Don't forget about the
+  // amount multiplier!
+  $settings['amount'] = ((int) $context['form']['amount'] * (int) $context['form']['qty']) * $context['component']['extra']['amount_value_multiplier'];
+
+  // Do something only at checkout but not when processing Stripe token.
+  if ($context['state'] === WEBFORM_STRIPE_SETTINGS_CHECKOUT) {
+    // Can't think of an example!
   }
 }
